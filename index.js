82804ea8fb8ac4609304cd54f70ce5ce2ca6f4e3
Cody Chan <int64ago@gmail.com>
2017-03-14 23:45:48 +0800

    feat: add permission control

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const ip = require('ip');
 const express = require('express');
 const { createRequestHandler } = require('express-unpkg');
 
@@ -9,9 +10,27 @@ const unpkg = createRequestHandler({
   autoIndex: true,
 });
 
+const isInWhiteList = function(referer) {
+  if (!referer) return true;
+  return [
+    /kaola\.com$/,
+    /netease\.com$/,
+    /163\.com$/,
+  ].some(function(d) {
+    return d.test(referer);
+  });
+}
+
+app.use(function(req, res, next) {
+  if (ip.isPrivate(req.ip)) return next();
+  if (isInWhiteList(req.get('Referer'))) return next();
+
+  res.status(403).send('403 Forbidden');
+});
+
 app.use(unpkg);
 
 const port = process.env.PORT || 8088;
-app.listen(port, function() {
+app.listen(port, '0.0.0.0', function() {
   console.log(`Start server on port: ${port}`);
 });
