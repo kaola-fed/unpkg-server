4a88947309185201d8e753cfbbc5269bd9e7f2e4
Cody Chan <int64ago@gmail.com>
2017-03-25 16:45:22 +0800

    feat: add ci token

--- middleware/webhook.js
+++ middleware/webhook.js
@@ -1,12 +1,13 @@
 const db = require('../utils/db');
 
-const CI_PASS = process.argv[5] || 'ci';
+const CI_TOKEN = process.argv[5] || 'ci';
 
 module.exports = function(req, res, next) {
+  if (req.get('X-Gitlab-Token') !== CI_TOKEN) {
+    return res.send(':-)');
+  }
+
   if (/get/i.test(req.method)) {
-    if (req.get('X-CI-Header') !== CI_PASS) {
-      return res.send(':-)');
-    }
     db.get(function(err, tasks) {
       res.json(tasks || []);
     })
